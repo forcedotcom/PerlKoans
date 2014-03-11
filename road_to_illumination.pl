ea4400e7b7ea89e258c49d1f72e4502551044e59
Conor Horan-Kates <choran-kates@salesforce.com>
2014-03-10 22:12:39 -0700

    cleaning up trailing spaces

--- road_to_illumination.pl
+++ road_to_illumination.pl
@@ -2,20 +2,20 @@
 =pod
     Copyright (c) 2012, Salesforce.com, Inc.
     All rights reserved.
-    
+
     Redistribution and use in source and binary forms, with or without modification,
     are permitted provided that the following conditions are met:
-    
+
     Redistributions of source code must retain the above copyright notice, this list
     of conditions and the following disclaimer. Redistributions in binary form must
     reproduce the above copyright notice, this list of conditions and the following
     disclaimer in the documentation and/or other materials provided with the
     distribution.
-    
+
     Neither the name of salesforce.com, inc. nor the names of its contributors may be
     used to endorse or promote products derived from this software without specific
     prior written permission.
-    
+
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -62,14 +62,14 @@ BEGIN {
 # 'about_file_find.pl'; # good real world example of callbacks and sub refs
 
 is (Perl::Koans::Koans::run(),      0, 'Perl::Koans::Koans'); # http://is.gd/C34W25
-is (Perl::Koans::Truth::run(),      0, 'Perl::Koans::Truth'); 
+is (Perl::Koans::Truth::run(),      0, 'Perl::Koans::Truth');
 is (Perl::Koans::Defined::run(),    0, 'Perl::Koans::Defined');
-is (Perl::Koans::Variables::run(),  0, 'Perl::Koans::Variables'); 
-is (Perl::Koans::Math::run(),       0, 'Perl::Koans::Math'); 
-is (Perl::Koans::Time::run(),       0, 'Perl::Koans::Time'); 
+is (Perl::Koans::Variables::run(),  0, 'Perl::Koans::Variables');
+is (Perl::Koans::Math::run(),       0, 'Perl::Koans::Math');
+is (Perl::Koans::Time::run(),       0, 'Perl::Koans::Time');
 is (Perl::Koans::IO::run(),         0, 'Perl::Koans::IO');
 is (Perl::Koans::Functions::run(),  0, 'Perl::Koans::Functions');
-is (Perl::Koans::Files::run(),      0, 'Perl::Koans::Files'); 
+is (Perl::Koans::Files::run(),      0, 'Perl::Koans::Files');
 is (Perl::Koans::Scope::run(),      0, 'Perl::Koans::Scope');
 is (Perl::Koans::Control::run(),    0, 'Perl::Koans::Control');
 is (Perl::Koans::Loops::run(),      0, 'Perl::Koans::Loops');
